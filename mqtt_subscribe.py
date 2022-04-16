5b693d66d77962a064a7df38d63e675000ca8a4f
Sebastian Ehrhardt <sebastian.ehrhardt@telekom.de>
2022-04-16 11:08:23 +0200

    first commit

---
+++ mqtt_subscribe.py
@@ -0,0 +1,25 @@
+import paho.mqtt.client as mqtt
+import json
+
+
+def on_message(client, userdata, message):
+    data = json.loads(message.payload.decode("utf-8"))
+    # print("message received ", str(message.payload.decode("utf-8")))
+    # print("message topic=", message.topic)
+    if 'action' in data:
+        if data['action'] == 'shortPress':
+            print('Short Press')
+        if data['action'] == 'longPress':
+            print('Long Press')
+    if 'open' in data:
+        if data['open'] is True:
+            print('Door is open!')
+
+
+mqttBroker = "172.24.1.8"  # ip of the server
+client = mqtt.Client('my-mqtt-client')  # create instance
+client.connect(mqttBroker)  # connect to broker
+client.subscribe("meraki/v1/mt/#")  # subscribe to topic
+
+client.on_message = on_message  # call function for messages
+client.loop_forever()  # start an endless loop
